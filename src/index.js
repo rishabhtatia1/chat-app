101b8296259f0352fe39e8fb113e33f3d7c75a20
rishabhtatia <rishabh.tatia@codeastu.com>
2021-08-08 21:27:48 +0530

    Added broadcasting events and config for socketio

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const { message } = require("statuses");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+const port = process.env.PORT || 3000;
+const publicDirectories = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectories));
+
+io.on("connection", (socket) => {
+  console.log("New connection");
+  socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new user joined!!");
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left");
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on ${port}`);
+});
